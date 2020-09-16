d1480545f8567bf87e375e714816c742db84588f
Sunrise <sunrisewng@gmail.com>
2020-09-16 15:56:56 +0200

    general MCTS code

--- src/integrators/mcts/mctstree.cpp
+++ src/integrators/mcts/mctstree.cpp
@@ -1,19 +1,110 @@
 #include "mctstree.h"
 
+#include <stack>
+#include <unordered_map>
+
 MTS_NAMESPACE_BEGIN
+
+MCTSTreeNode::MCTSTreeNode(std::uint32_t num_children) : 
+    children(num_children, nullptr),
+    prob_acc(0.f),
+    val(0.f),
+    visited(0){
+}
+
+MCTSTreeNode::~MCTSTreeNode(){
+
+}
 
 MCTSTree::MCTSTree(const std::pair<Point2i, Vector2i>& sensor_area, ImageBlock* wr, std::unique_ptr<SelectionPolicy> spol, 
         std::unique_ptr<DefaultPolicy> dpol, std::unique_ptr<PathGenerator> pathgen, std::unique_ptr<ContributionCalculator> contrib, 
-        std::unique_ptr<NodeDiscretizer> ndisc){
-
+        std::unique_ptr<NodeDiscretizer> ndisc) : 
+            sensor_area_(sensor_area), 
+            wr_(wr), 
+            spol_(std::move(spol)), 
+            dpol_(std::move(dpol)),
+            pathgen_(std::move(pathgen)),
+            contrib_(std::move(contrib)),
+            ndisc_(std::move(ndisc)){
+    std::uint32_t root_children = ndisc_->getNumChildren(sensor_area_);
+    root_ = std::unique_ptr<MCTSTreeNode>(new MCTSTreeNode(root_children));
 }
 
 MCTSTree::~MCTSTree(){
 
 }
 
-void MCTSTree::iterate(std::mutex& wr_mutex){
-    
+void MCTSTree::iterate(){
+    std::vector<std::uint32_t> visited_children;
+    std::vector<float> probabilities;
+
+    //selection and expansion
+    std::vector<Intersection> path = pathgen_->GeneratePath(sensor_area_, root_.get(), spol_, ndisc_, visited_children, probabilities);
+
+    //simulate from last node to obtain result
+    Spectrum simulated = dpol_->simulate(path.back());
+    std::stack<MCTSTreeNode*> visited_nodes;
+
+    //backpropagation to update stats
+    MCTSTreeNode* curr = root_;
+    for(std::uint32_t i = 0; i < visited_children.size(); ++i){
+        curr = curr->children[visited_children[i]];
+        visited_nodes.push_back(curr);
+    }
+
+    while(!visited_nodes.empty()){
+        curr = visited_nodes.pop_back();
+        simulated = computeContribution(path.back(), simulated);
+        curr->prob_acc += probabilities.back();
+        probabilities.pop_back();
+        curr->val += simulated;
+        curr->visited++;
+    }
+}
+
+void MCTSTree::develop(std::mutex& wr_mutex){
+    Vector2i size = wr_->getSize();
+    std::unordered_map<std::uint32_t, std::uint32_t> total_sampled;
+    std::unordered_map<std::uint32_t, Spectrum> final_pixel_values;
+
+    for(std:uint32_t i = 0; i < root_->children.size(); ++i){
+        if(root_->children[i] == nullptr){
+            continue;
+        }
+        Point2i pixel = ndisc_->getChildSensorCoord(sensor_area_, i);
+        std::uint32_t pixel_idx = pixel.x + pixel.y * size.x;
+
+        if(total_sampled.find(pixel_idx) != total_sampled.end()){
+            total_sampled[pixel_idx] += root_->children[i]->visited;
+        }
+        else total_sampled[pixel_idx] = root_->children[i]->visited;
+    }
+
+    for(std:uint32_t i = 0; i < root_->children.size(); ++i){
+        if(root_->children[i] == nullptr){
+            continue;
+        }
+
+        Point2i pixel = ndisc_->getChildSensorCoord(sensor_area_, i);
+        std::uint32_t pixel_idx = pixel.x + pixel.y * size.x;
+
+        Spectrum child_contrib = root_->children[i]->val / root_->children[i]->prob_accum * 
+            ((float)root_->children[i]->visited / total_sampled[pixel_idx]);
+
+        if(final_pixel_values.find(pixel_idx) != final_pixel_values.end()){
+            final_pixel_values[pixel_idx] += child_contrib;
+        }
+        else final_pixel_values[pixel_idx] = child_contrib;
+    }
+
+    {
+        std::lock_guard<std::mutex> lock(wr_mutex);
+
+        for(auto iter = final_pixel_values.begin(); iter != final_pixel_values.end(); ++iter){
+            Point2i pixel(iter->first % size.x, iter->first / size.x);
+            wr_->put(pixel, iter->second);
+        }
+    }
 }
 
 MTS_NAMESPACE_END
--- src/integrators/mcts/mctstree.h
+++ src/integrators/mcts/mctstree.h
@@ -10,12 +10,15 @@ MTS_NAMESPACE_BEGIN
 
 class MCTSTreeNode{
 public:
-    MCTSTreeNode();
-    MCTSTreeNode(int num_children);
-    MCTSTreeNode(const MCTSTreeNode& other);
+    MCTSTreeNode() = delete;
+    MCTSTreeNode(std::uint32_t num_children);
+    MCTSTreeNode(const MCTSTreeNode& other) = delete;
     ~MCTSTreeNode();
 
-    std::vector<std::pair<std::unique_ptr<MCTSTreeNode>, float>> children_;
+    std::vector<std::unique_ptr<MCTSTreeNode> children;
+    float prob_acc;
+    Spectrum val;
+    std::uint32_t visited;
 };
 
 class SelectionPolicy{
@@ -33,13 +36,14 @@ public:
     virtual std::uint32_t getNumChildren(const std::pair<Point2i, Vector2i>& sensor_area) = 0;
     virtual std::uint32_t getNumChildren(const Intersection& its) = 0;
     virtual std::pair<Vector2f, Vector2f> getChildItsCoordRange(const Intersection& its, std::uint32_t child) = 0;
-    virtual std::pair<Vector2i, Vector2i> getChildSensorCoordRange(const std::pair<Point2i, Vector2i>& sensor_area, std::uint32_t child) = 0;
+    virtual Point2i getChildSensorCoord(const std::pair<Point2i, Vector2i>& sensor_area, std::uint32_t child) = 0;
 };
 
 class PathGenerator{
 public:
-    virtual std::vector<Intersection> GeneratePath(const std::pair<Point2i, Vector2i>& sensor_area, 
-        MCTSTreeNode* root, SelectionPolicy* spol, NodeDiscretizer* ndisc) = 0;
+    virtual std::vector<Intersection> generatePath(const std::pair<Point2i, Vector2i>& sensor_area, 
+        MCTSTreeNode* root, SelectionPolicy* spol, NodeDiscretizer* ndisc, std::vector<std::uint32_t>& child_indices,
+        std::vector<float>& probabilities) = 0;
 };
 
 class ContributionCalculator{
@@ -55,7 +59,8 @@ public:
         std::unique_ptr<NodeDiscretizer> ndisc);
     ~MCTSTree();
 
-    void iterate(std::mutex& wr_mutex);
+    void iterate();
+    void develop(std::mutex& wr_mutex);
 private:
     std::pair<Point2i, Vector2i> sensor_area_;
     ImageBlock* wr_;
@@ -64,6 +69,8 @@ private:
     std::unique_ptr<PathGenerator> pathgen_;
     std::unique_ptr<ContributionCalculator> contrib_;
     std::unique_ptr<NodeDiscretizer> ndisc_;
+
+    std::unique_ptr<MCTSTreeNode> root_;
 };
 
 MTS_NAMESPACE_END
